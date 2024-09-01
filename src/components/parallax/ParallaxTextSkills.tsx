4f33f30879260f986d199cc6b9ed3a92f47f7d3e
znDev0101 <zulfanurhuda01@gmail.com>
2024-09-01 17:49:39 +0700

    fix warning

--- src/components/parallax/ParallaxTextSkills.tsx
+++ src/components/parallax/ParallaxTextSkills.tsx
@@ -41,7 +41,7 @@ const ParallaxTextSkills = ({
   const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
 
   const directionFactor = useRef<number>(1);
-  useAnimationFrame((t, delta) => {
+  useAnimationFrame((delta) => {
     let moveBy = directionFactor.current * baseVelocity * (delta / 1200);
 
     /**
